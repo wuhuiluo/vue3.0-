dcf9aafd4180369f756380d5418231071d24ac2f
wuhuiluo <510426343@qq.com>
2020-12-14 14:34:28 +0800

    latest commit

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,7 @@
   <footer class="text-center py-4 text-secondary bg-light mt-auto">
     <small>
       <ul class="list-inline mb-0">
-        <li class="list-inline-item">@ 2020 者也专栏</li>
-        <li class="list-inline-item">课程</li>
-        <li class="list-inline-item">文档</li>
-        <li class="list-inline-item">联系</li>
-        <li class="list-inline-item">更多</li>
+        <li class="list-inline-item">@ 2020 帅气专栏</li>
       </ul>
     </small>
   </footer>
--- vue.config.js
+++ vue.config.js
@@ -4,17 +4,18 @@ module.exports = {
         host: 'localhost',
         port: 8080,
         https: false,
+        disableHostCheck: true,
         //以上的ip和端口是我们本机的;下面为需要跨域的
-        proxy: {//配置跨域
+        proxy: { //配置跨域
             '/api': {
-                target: 'http://api.vikingship.xyz/',//这里后台的地址模拟的;应该填写你们真实的后台接口
+                target: 'http://api.vikingship.xyz/', //这里后台的地址模拟的;应该填写你们真实的后台接口
                 ws: true,
-                changOrigin: true,//允许跨域
+                changOrigin: true, //允许跨域
                 pathRewrite: {
-                    '^/api': ''//请求的时候使用这个api就可以
+                    '^/api': '' //请求的时候使用这个api就可以
                 }
             }
-            
+
         }
     }
 }
